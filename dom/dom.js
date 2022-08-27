a2c5d3fe410fa4a3017b7402028cd8460b094cb1
Marcos Veron <marcosveronar@gmail.com>
2022-08-27 06:42:33 -0300

    Agregar .gitignore y primeros archivos del proyecto

---
+++ dom/dom.js
@@ -0,0 +1,18 @@
+const h1 = document.querySelector('h1')
+const form = document.querySelector('#formulario')
+const p = document.querySelector('p')
+const input1 = document.getElementById('calculo1')
+const input2 = document.getElementById('calculo2')
+const btn = document.getElementById('btnCalcular')
+const pResultado = document.getElementById('resultado')
+
+// form.addEventListener('submit', sumarInput)
+
+// function sumarInput(event) {
+//    let resultado = Number(input1.value) + Number(input2.value)
+//    pResultado.innerHTML= resultado
+//    event.preventDefault();
+// }
+
+
+
